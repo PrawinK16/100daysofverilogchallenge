338ed87f4db4e5ad77415037f29d94bfd85c8cf8
PrawinK16 <120790887+PrawinK16@users.noreply.github.com>
2024-09-16 16:00:20 +0530

    Add files via upload

---
+++ 8*16 Synchronous single port RAM/RAM_tb.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/13/2024 02:59:41 PM
+// Design Name: 
+// Module Name: RAM_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RAM_tb( );
+
+    reg 
+
+endmodule
