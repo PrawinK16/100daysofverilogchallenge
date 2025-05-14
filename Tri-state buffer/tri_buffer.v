6ec9eaa0d76e7209e36013612e1a2460c64df641
Prawin Krishnamoorthy <120790887+PrawinK16@users.noreply.github.com>
2025-05-14 17:13:35 +0530

    Update tri_buffer.v

--- Tri-state buffer/tri_buffer.v
+++ Tri-state buffer/tri_buffer.v
@@ -1,1 +1,16 @@
+`timescale 1ns / 1ps
+
+module tri_buffer(
+  input a,ctrl,
+  output reg out
+    );
+    
+always@(*)
+begin
+  if(ctrl)
+    out <= a;
+  else
+    out <= 1'bz; 
+end
+endmodule
 
