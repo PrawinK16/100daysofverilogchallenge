e46e44dfceae990bc39192f3307e239b3af4256c
Prawin Krishnamoorthy <120790887+PrawinK16@users.noreply.github.com>
2025-06-19 11:15:14 +0530

    Create johnson_counter_tb.v

---
+++ 4 - bit Johnson Counter/johnson_counter_tb.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+
+module johnson_counter_tb(
+
+    );
+    reg clk,rst;
+    wire [3:0]q;
+    
+    johnson_counter dut(.clk(clk),.rst(rst),.q(q));
+    
+    initial
+    begin
+    clk =0;
+    forever #5 clk = ~clk;
+    end
+    
+    initial 
+    begin 
+    rst =1;
+    #10;
+    rst = 0;
+    #100 $finish;
+    end
+    endmodule
+
